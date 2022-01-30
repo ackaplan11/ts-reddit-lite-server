56fabd8a64ffbbf13054346562af203b6d13d531
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-01-29 20:09:54 -0800

    databse connection established, prisma client instantiated

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,16 @@
-console.log("hello world");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+async function main() {
+    const allUsers = await prisma.user.findMany();
+    console.log(allUsers);
+}
+main()
+    .catch((e) => {
+    throw e;
+})
+    .finally(async () => {
+    await prisma.$disconnect();
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const allUsers = await prisma.user.findMany()
+    console.log(allUsers)
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
